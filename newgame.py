22ed024432f08b70bf59fc2586d14283e4f9aab1
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-21 18:59:23 -0400

    Updated from Brython Server: 3/21/2017 6:59:22 PM

--- newgame.py
+++ newgame.py
@@ -1,1 +1,14 @@
+from ggame import App, RectangleAsset, ImageAsset, SoundAsset
+from ggame import LineStyle, Color, Sprite, Sound
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+noline = LineStyle(0.5, black)
+grass=ImageAsset("images/grass_texture239.jpg")
+grassimage=Sprite(grass, (0,0))
+
+myapp = App()
+myapp.run()
