f57bf91e1b86679c503bc8e437fac9a6e4a17e1e
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-04-28 14:54:34 -0400

    Updated from Brython Server: 4/28/2017 2:54:33 PM

--- Iwanttobelieve.py
+++ Iwanttobelieve.py
@@ -52,10 +52,12 @@ class Plane(Sprite):
 class Game(App):
     def __init__(self, width, height):
         super().__init__(width, height)
+        """
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
+        """
         Field((0,0))
         Plane((100,100))
         
